79530ed51df620f9416cbcbe8e81340e4406e5f9
microsoftreact <microsoftreact@gmail.com>
2024-11-12 17:02:53 +0500

    Javascriptda o'zgarish bo'ldi

---
+++ js/script.js
@@ -0,0 +1,56 @@
+let start = 10;
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
+btn.addEventListener("click", () => {
+  console.log(start);
+});
